e1a4df205ae6cbc8706d857bfbd2e24d7bff190a
Legic <Legic2007@yandex.ru>
2024-01-17 03:28:54 +0300

    initial commit

---
+++ src/main/java/ru/netology/springboot/controller/ProfileController.java
@@ -0,0 +1,23 @@
+package ru.netology.springboot.controller;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.netology.springboot.SystemProfile;
+
+@RestController
+@ConfigurationProperties("netology.profile")
+@RequestMapping("/")
+public class ProfileController {
+    private SystemProfile profile;
+
+    public ProfileController(SystemProfile profile) {
+        this.profile = profile;
+    }
+
+    @GetMapping("profile")
+    public String getProfile() {
+        return profile.getProfile();
+    }
+}
